560b0e3868abb4561614776f89ea4a3d67dd793c
Rodrigo Tagliari <rodrigotagliarii@gmail.com>
2021-12-31 16:17:29 -0300

    first commit

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Project;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(),
+            'project_id' => Project::factory(),
+            'name' => $this->faker->word(),
+        ];
+    }
+}
